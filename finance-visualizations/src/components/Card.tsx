18a6ab78ee1503b441f4bede69eeb23c2a51f7f2
AlC0DEMAGO <cesaralatorre22@gmail.com>
2025-08-22 13:43:17 -0600

    arguments props

--- finance-visualizations/src/components/Card.tsx
+++ finance-visualizations/src/components/Card.tsx
@@ -1,18 +1,29 @@
-function Card() {
+interface Props {
+  body: string;
+}
+
+function Card(props: Props) {
+  const { body } = props; // {body}
   const width = { width: "350px" };
   return (
     <div className="card" style={width}>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card’s content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
-      </div>
+      <div className="card-body">{body}</div>
     </div>
+  );
+}
+
+export function CardBody() {
+  return (
+    <>
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card’s content.
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+    </>
   );
 }
 
