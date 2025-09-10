66e37a58fe176ff83079d05675b07f4b46dfaa8f
AlC0DEMAGO <cesaralatorre22@gmail.com>
2025-09-10 17:32:55 -0600

    print lists and event management

--- finance-visualizations/src/App.tsx
+++ finance-visualizations/src/App.tsx
@@ -1,9 +1,11 @@
 import Card, { CardBody } from "./components/Card";
-
+import List from "./components/List";
 function App() {
+  const list = ["Pikachu","Snorlax","Bulbasur"];
   return (
     <Card>
       <CardBody title="Titulo" text="este es el texto" />
+      <List data={list}></List>
     </Card>
   );
 }
