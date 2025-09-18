495e41f00d8989a6fadcc2e7ed62a9f6c4654b93
AlC0DEMAGO <cesaralatorre22@gmail.com>
2025-09-18 15:56:55 -0600

    state management

--- finance-visualizations/src/App.tsx
+++ finance-visualizations/src/App.tsx
@@ -1,7 +1,7 @@
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 function App() {
-  const list = ["Pikachu","Snorlax","Bulbasur"];
+  const list = ["Pikachu", "Snorlax", "Bulbasur"];
   return (
     <Card>
       <CardBody title="Titulo" text="este es el texto" />
