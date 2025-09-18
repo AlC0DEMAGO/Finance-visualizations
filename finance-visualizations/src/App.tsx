684daae07c230a0de84ad593ed71fc1f289e35c5
AlC0DEMAGO <cesaralatorre22@gmail.com>
2025-09-18 16:04:29 -0600

    Functions as a props

--- finance-visualizations/src/App.tsx
+++ finance-visualizations/src/App.tsx
@@ -2,10 +2,20 @@ import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 function App() {
   const list = ["Pikachu", "Snorlax", "Bulbasur"];
+
+  const handleSelect = (elemento: string) => {
+    console.log("imprimiento:", elemento);
+  };
+
+  const handleSelect2 = (elemento: string) => {
+    console.log("este es: ", elemento);
+  };
+
   return (
     <Card>
       <CardBody title="Titulo" text="este es el texto" />
-      <List data={list}></List>
+      <List data={list} onSelect={handleSelect}></List>
+      <List data={list} onSelect={handleSelect2}></List>
     </Card>
   );
 }
